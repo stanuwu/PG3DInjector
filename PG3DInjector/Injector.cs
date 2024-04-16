b1139c85a402fac98850ff0b5329b4a4a7cb7bcb
Patrick Thompson <patrick@dontsp.am>
2024-04-15 22:34:30 -0700

    Update to use manual mapping - CBA to implement manual mapping myself so used: https://github.com/jdewera/lunar

---
+++ PG3DInjector/Injector.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Lunar;
+
+namespace PG3DInjector
+{
+    public class Injector
+    {
+        public static void Map(string process, string dll)
+        {
+            Process[] processes = Process.GetProcessesByName(process);
+            if (processes.Length == 0)
+            {
+                Logger.ConsoleWrite("[<ERROR>] Process not found.", ConsoleColor.Red);
+                return;
+            }
+
+            try
+            {
+                var mapper = new LibraryMapper(processes[0], dll, MappingFlags.DiscardHeaders);
+                mapper.MapLibrary();
+            } catch (Exception ex)
+            {
+                Logger.ConsoleWrite($"[<ERROR>] {ex.Message}", ConsoleColor.Red);
+            }
+        }
+    }
+}

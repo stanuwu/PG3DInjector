2f7b65ab92207a8fa3a95e0d408ba46366405f74
Sympact <47295195+sympact06@users.noreply.github.com>
2024-04-30 17:50:45 +0200

    Refactored Injector class and error handling
    The Injector class has been refactored for improved readability. The namespace declaration has been simplified, and the Map method's structure has been streamlined. Error handling for when a process is not found has also been enhanced to provide clearer feedback.

--- PG3DInjector/Injector.cs
+++ PG3DInjector/Injector.cs
@@ -1,21 +1,20 @@
 ﻿using System.Diagnostics;
 using Lunar;
 
-namespace PG3DInjector
-{
-    public class Injector
-    {
-        public static void Map(string process, string dll)
-        {
-            Process[] processes = Process.GetProcessesByName(process);
-            if (processes.Length == 0)
-            {
-                Logger.ConsoleWrite("[<ERROR>] Process not found.", ConsoleColor.Red);
-                return;
-            }
+namespace PG3DInjector;
 
-            var mapper = new LibraryMapper(processes[0], dll, MappingFlags.DiscardHeaders);
-            mapper.MapLibrary();
+public class Injector
+{
+    public static void Map(string process, string dll)
+    {
+        Process[] processes = Process.GetProcessesByName(process);
+        if (processes.Length == 0)
+        {
+            Logger.ConsoleWrite("[<ERROR>] Process not found.", ConsoleColor.Red);
+            return;
         }
+
+        var mapper = new LibraryMapper(processes[0], dll, MappingFlags.DiscardHeaders);
+        mapper.MapLibrary();
     }
 }
